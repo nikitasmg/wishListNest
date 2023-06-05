0339daac9072c8c4fe89056dbcd27c7500256e43
Nikita Smagin <n.smagin@agimagroup.ru>
2023-06-05 17:41:03 +0500

    change db

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,10 +3,10 @@ import { TypeOrmModuleOptions} from '@nestjs/typeorm'
 
 export const getProgressConfig = async (config: ConfigService): Promise<TypeOrmModuleOptions> =>({
     type: 'postgres',
-    host: 'dpg-ceqko7g2i3mov0i1m1q0-a.frankfurt-postgres.render.com',
-    username: 'wishlistdb_user',
-    password: 'Z4XdOtUjgW9CCdtQivvuFybtcmcCgHte',
-    database: 'wishlistdb',
+    host: 'dpg-chutd15269vccp1j3pjg-a.frankfurt-postgres.render.com',
+    username: 'wish_list_user',
+    password: 'YcFBeXq53oxQ44UFVWhM0hlHAoLMz2ZB',
+    database: 'wish_list',
     port: 5432,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
