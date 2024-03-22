2b8510345fb8f41b649552be2301654a0330e6ef
nvsmagin <nvsmagin@gid.ru>
2024-03-23 01:30:22 +0500

    Настройка порта

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = await app.get(ConfigService)
   app.enableCors()
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3001
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
